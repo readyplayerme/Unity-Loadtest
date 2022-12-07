1fc8361568ac3b3f3ee4182d1d31d155083b5848
Bernhard Fercher <42868289+bfercher@users.noreply.github.com>
2022-12-07 16:51:04 +0100

    Fixed WebGL CSV loading (#8)

--- Assets/Scripts/AvatarIDReader.cs
+++ Assets/Scripts/AvatarIDReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -6,13 +7,15 @@ namespace ReadyPlayerMe.Loadtest {
     public class AvatarIDReader
     {
         public List<string> AvatarList { get; private set; }
-        public void FromCSV(string fileName)
+
+        public void ReadCSVFromResources(string fileName)
         {
             AvatarList = new List<string>();
-            var streamReader = new StreamReader(fileName);
-            var data = streamReader.ReadToEnd();
-            Debug.Log(data);
-            AvatarList.AddRange(data?.Split(','));
+            
+            var txt = Resources.Load(fileName) as TextAsset;
+            var data = txt?.text;
+            
+            AvatarList.AddRange(data?.Split(',') ?? Array.Empty<string>());
         }
     }
 }
